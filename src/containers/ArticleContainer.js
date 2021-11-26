4582d7024e72e06c1d85361e10fe7a75f7abc8b6
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2021-11-26 14:22:30 +0000

    API fetch data correctly displays

--- src/containers/ArticleContainer.js
+++ src/containers/ArticleContainer.js
@@ -1,6 +1,18 @@
-import react from 'react';
+import react, {useState, useEffect} from 'react';
 
 const ArticleContainer = () => {
+
+    const [newsArticles, setNewsArticles] = useState([])
+
+    useEffect(() =>{
+        getNewsArticles()
+    }, [])
+
+    const getNewsArticles = () => {
+        fetch("https://content.guardianapis.com/search?q=brexit&format=json&api-key=test")
+        .then(response => response.json())
+        .then(data => setNewsArticles(data))
+    }
 
     return (
         <h2>hello world</h2>
